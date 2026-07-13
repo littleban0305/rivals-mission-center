1339ca0db3a081279f5c3cdcde48b2704bed06ef
littleban0305 <littleban0305@gmail.com>
2026-07-13 20:54:26 +0800

    Create database.js

---
+++ js/database.js
@@ -0,0 +1,21 @@
+const SHOP_ITEMS = {
+    SC001: {
+        name: "Skin Case",
+        rPrice: 249,
+        goldPrice: 1000,
+        image: "images/skincase.png"
+    },
+
+    SC002: {
+        name: "椰子鐮刀",
+        rPrice: 799,
+        goldPrice: 3200,
+        image: "images/coconut-scythe.png"
+    }
+};
+
+const MISSIONS = {
+    D001: {...},
+    D002: {...},
+    W001: {...}
+};
