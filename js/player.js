acc1bf0aa7cb5d7aeef7995e5f4b29880bbcf8ba
littleban0305 <littleban0305@gmail.com>
2026-07-14 13:14:55 +0800

    Update player.js

--- js/player.js
+++ js/player.js
@@ -3,15 +3,13 @@
 // Player Center
 // ==========================
 
+// ==========================
 // 讀取玩家資料
+// ==========================
+
 const playerData = JSON.parse(localStorage.getItem("player"));
 
 if (!playerData || localStorage.getItem("isLogin") !== "true") {
-
-    window.location.href = "login.html";
-
-}
-if (!playerData || loginStatus !== "true") {
 
     window.location.href = "login.html";
 
