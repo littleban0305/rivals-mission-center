1fee6e03100a8c0bb6f2d3a733a23c1bbde4ddef
littleban0305 <littleban0305@gmail.com>
2026-07-17 07:36:33 +0800

    Update database.js

--- js/database.js
+++ js/database.js
@@ -3,20 +3,20 @@ const SHOP_ITEMS = {
         name: "Skin Case",
         rPrice: 249,
         goldPrice: 1000,
-        image: "images/skincase.png"
+        image: "images/items/skincase.png"
     },
 
     SC002: {
         name: "椰子鐮刀",
         rPrice: 799,
         goldPrice: 3200,
-        image: "images/coconut-scythe.png"
+        image: "images/items/coconut-scythe.png"
     },
 
     SC003: {
         name: "漫畫閱讀券 ×1",
         rPrice: 69,
         goldPrice: 200,
-        image: "images/comic-ticket.png"
+        image: "images/items/comic-ticket.png"
     }
 };
