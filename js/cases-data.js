84e0f842a038425c800ee74d0d15bcc1022e0cb3
littleban0305 <littleban0305@gmail.com>
2026-08-03 11:08:48 +0800

    Create cases-data.js

---
+++ js/cases-data.js
@@ -0,0 +1,92 @@
+// ==========================
+// Rivals Mission Center
+// Cases Data Beta 1
+// ==========================
+
+const cases = {
+
+    CASE001: {
+
+        name: "Starter Case",
+
+        image:
+            "images/cases/starter.png",
+
+        price: 500,
+
+        desc:
+            "適合新手的基本箱子。",
+
+        items: [
+
+            {
+
+                id: "GOLD50",
+
+                chance: 40
+
+            },
+
+            {
+
+                id: "GOLD100",
+
+                chance: 30
+
+            },
+
+            {
+
+                id: "PALM_SCYTHE",
+
+                chance: 20
+
+            },
+
+            {
+
+                id: "RED_GUN",
+
+                chance: 10
+
+            }
+
+        ]
+
+    },
+
+    CASE002: {
+
+        name: "Legend Case",
+
+        image:
+            "images/cases/legend.png",
+
+        price: 1500,
+
+        desc:
+            "高級收藏箱。",
+
+        items: [
+
+            {
+
+                id: "GOLD500",
+
+                chance: 60
+
+            },
+
+            {
+
+                id: "RED_GUN",
+
+                chance: 40
+
+            }
+
+        ]
+
+    }
+
+};
