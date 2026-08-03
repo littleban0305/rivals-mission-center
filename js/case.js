9c0d1aca47c7d8cefa96acc25452c553ba7db20c
littleban0305 <littleban0305@gmail.com>
2026-08-03 13:00:08 +0800

    Update case.js

--- js/case.js
+++ js/case.js
@@ -25,3 +25,47 @@ document
 .getElementById("caseName")
 .textContent =
 box.name;
+
+function randomItem(itemList) {
+
+    const total =
+        itemList.reduce(
+            (sum, item) =>
+                sum + item.chance,
+            0
+        );
+
+    let random =
+        Math.random() * total;
+
+    for (const item of itemList) {
+
+        random -= item.chance;
+
+        if (random <= 0) {
+
+            return items[item.id];
+
+        }
+
+    }
+
+}
+
+document
+.getElementById("openCase")
+.addEventListener(
+    "click",
+    () => {
+
+        const reward =
+            randomItem(
+                box.items
+            );
+
+        alert(
+            `🎉 恭喜獲得 ${reward.name}`
+        );
+
+    }
+);
